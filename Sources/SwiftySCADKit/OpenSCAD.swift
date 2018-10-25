161d03b5e2d3ae2b38e2cd663e7e5410c9874c1c
Sam <samgauck@icloud.com>
2018-10-25 11:03:44 -0400

    Add base OpenSCAD docs

--- Sources/SwiftySCADKit/OpenSCAD.swift
+++ Sources/SwiftySCADKit/OpenSCAD.swift
@@ -1,8 +1,23 @@
 import Foundation
 
+/// A representation of an OpenSCAD object.
+///
+/// The recommended way to create an OpenSCAD object is by using one of these static functions:
+/// - cube(withSideLength:centered:)
+/// - cube(height:width:depth:centered:)
+/// - sphere(_:)
+/// - cylinder(height:topRadius:bottomRadius:centered:)
+/// - polyhedron(faces:)
+/// - triangularPrism(bottom:top:)
 public struct OpenSCAD {
+    /// The raw OpenSCAD value of this object.
     public var SCADValue: String = ""
+    /// This initializer is private so that it cannot be used.
     private init() {}
+    /// Creates a new OpenSCAD struct with the specified OpenSCAD raw value.
+    /// **NOTE**: This raw value is **UNCHECKED**! If it is wrong, ALL YOUR OPENSCAD WON'T WORK!
+    ///
+    /// - Parameter SCAD: The raw value of this OpenSCAD struct.
     public init(_ SCAD: String) {
         SCADValue = SCAD
     }
