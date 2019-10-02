37b858891fb6324609bb1d12d2fbb613f92ac41e
Sam <samgauck@icloud.com>
2019-10-02 07:36:33 -0400

    Use the swift version compiler directive that is supported pre-Swift 5

--- Sources/SwiftySCADKit/Import.swift
+++ Sources/SwiftySCADKit/Import.swift
@@ -6,12 +6,12 @@ public extension OpenSCAD {
     /// - Parameter path: The path to the file to import. It can be relative or absolute, but must be be local (i.e. not online)
     /// - Returns: The model that was imported
     public static func `import`(_ path: String) -> OpenSCAD {
-        #if swift(<5)
-        let url = URL(fileURLWithPath: path)
-        return OpenSCAD("import(\"\(url.standardized.path)\");\n")
-        #else
+        #if swift(>=5)
         let url = URL(fileURLWithPath: path)
         return OpenSCAD(#"import("\#(url.standardized.path)");\#n"#)
+        #else
+        let url = URL(fileURLWithPath: path)
+        return OpenSCAD("import(\"\(url.standardized.path)\");\n")
         #endif
     }
 }
